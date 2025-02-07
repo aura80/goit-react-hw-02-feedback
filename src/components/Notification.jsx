4e063cfbab451b6ed2d903c1c5fa9b9b377f0560
User <96388713+aura80@users.noreply.github.com>
2025-02-07 14:31:52 +0200

    Update Notification.jsx

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -1,7 +1,8 @@
 import PropTypes from 'prop-types';
+import styles from './Notification.module.css';
 
 const Notification = ({ message }) => (
-    <p className="{styles.notification">{message}</p>
+    <p className={styles.notification}>{message}</p>
 );
 
 Notification.propTypes = {
